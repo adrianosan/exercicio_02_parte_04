8cbbf160148c7021f7e2f680dcc01a2d6215ce09
adriano.santos <adriano.santos@squadra.com.br>
2019-05-30 13:45:52 -0300

    exercicio- 02- parte 04

---
+++ exercicio_02_ parte 04/exercicio_02_ parte 04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_02__parte_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            Console.Write("Digite o primeiro número. ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo número. ");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+
+            if (primeiroNumero > segundoNumero)
+            {
+                Console.WriteLine("primeiro número maior");
+            }
+
+            else
+            {
+                Console.WriteLine("segundo número maior");
+            }         
+        }        
+           
+
+    }
+}
